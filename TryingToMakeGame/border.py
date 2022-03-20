b29f5b670cd046768816bc533dd0cb86835d1d86
obvallata <dashreshet@mail.ru>
2022-03-20 14:32:50 +0300

    Up-to-date files and install programm

--- TryingToMakeGame/border.py
+++ TryingToMakeGame/border.py
@@ -3,5 +3,4 @@ from dragon import Dragon
 
 class Border(Dragon):
     def __init__(self, name, height, width, form):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite. Это очень важно!!!
         Dragon.__init__(self, name, height, width, form)
