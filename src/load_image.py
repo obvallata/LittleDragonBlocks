1bd10bfc387e5e9f18d3a0990f71f3b9bf8c556e
ozzzernova <55240621+ozzzernova@users.noreply.github.com>
2022-05-08 14:30:26 +0300

    Update load_image.py

--- src/load_image.py
+++ src/load_image.py
@@ -3,7 +3,7 @@ import pygame
 
 
 def load_image(name, height, width):
-    game_folder = os.path.dirname('LittleDragonBlocks')
+    game_folder = os.path.curdir
     img_folder = os.path.join(game_folder, 'img')
     image = pygame.image.load(os.path.join(img_folder, name)).convert()
     image.set_colorkey((0, 0, 0))
